faa4ca03969cc27a69f8b308d7de68054d8eaaac
dribeiroferr <dribeiroferr.ito@gmail.com>
2023-07-18 17:44:21 -0300

    infra: adding mongodb and make files for containers

---
+++ dev.env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#MongoDB secrets
+export MONGODB_USERNAME=admin
+export MONGODB_PASSWORD=admin
+export MONGODB_DATABASE=test_local_ai_generated_qrCode
+export MONGODB_PORT=27017
+export MONGODB_HOSTNAME=localhost
+export MONGODB_COLLECTION=aiQrCodeGeneratedImage
+export MONGODB_CONNECTION_STRING=mongodb://localhost/27017/test_local_ai_generated_qrCode
+
+#Stable Diffusion API settings
+export SDAPIKEY=UKjUm2r28SdN4MhthXmjjYXeX7V70gGzAxCulDy6kSgt0yYuhMbQNCx3lP0d
+
+#Application running PORT AND HOST
+export APPLICATION_HOSTNAME=localhost
+export APPLICATION_PORT=9196
